22e555c3a8b788b38f6322394e373b0db31e6598
devastian1 <kevindevastian.com>
2019-04-12 19:06:22 +0800

    changes

--- app/mailers/contacts_mailer.rb
+++ app/mailers/contacts_mailer.rb
@@ -6,7 +6,7 @@ class ContactsMailer < ActionMailer::Base
   
   def general_message(contact)
     @contact = contact
-    recipients = ["kevindevastian@gmail.com"," emir.shafiq@yahoo.com", 	"nurulhashidi@gmail.com", "	jeromedcruz5141@gmail.com" ]
+    recipients = ["kevindevastian@gmail.com"," emir.shafiq@yahoo.com", 	"nurulhashidi@gmail.com" ]
     make_bootstrap_mail( to: recipients, subject:  "You have a message from a  #{@contact.name} !!", from: "inquiry@newtimerezz.com", )
   end
 end
