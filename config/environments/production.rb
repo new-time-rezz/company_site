56924ac411c7d825ed32357fa7614b517e5d2b05
devastian1 <kevindevastian.com>
2019-04-12 14:13:12 +0800

    fixed minor bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://timerezz.herokuapp.com'}
 
-  config.action_mailer.delivery_method :smtp
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
